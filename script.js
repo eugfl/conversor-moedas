ed75acdc40184a6ca07989f5cf481362dae59779
Gabriel Figueiredo <eugfl@hotmail.com>
2023-04-10 11:03:54 -0300

    Adição da API

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 const button = document.getElementById("convert-button");
 const select = document.getElementById("currency-select");
 
-const dolar = 5.17;
-const euro = 5.60;
-const bitcoin = 0.0000071;
-
-const convertValue = () => {
+const convertValue = async () => {
 	const inputReais = document.getElementById("input-real").value;
 	const realValueText = document.getElementById("real-value-text");
 	const currencyValueText = document.getElementById("currency-value-text");
+
+	const data = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL").then(response => response.json());
+
+	const dolar = data.USDBRL.high;
+	const euro = data.EURBRL.high;
+	const bitcoin = data.BTCBRL.high;
+
 
 	realValueText.innerHTML = new Intl.NumberFormat("pt-BR", {
 		style: "currency",
@@ -30,7 +33,10 @@ const convertValue = () => {
 	}
 
 	if (select.value === "₿ Bitcoin") {
-		currencyValueText.innerHTML = inputReais * bitcoin;
+		currencyValueText.innerHTML = new Intl.NumberFormat("de-DE", {
+			style: "currency",
+			currency: "BTC",
+		}).format(inputReais / bitcoin);
 	}
 };
 
